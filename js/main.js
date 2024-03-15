3b3ccceb6e814f1b66f94ca5639425d37d6d2a36
NicolaBellini <bellininicola4366@gmail.com>
2024-03-15 11:20:52 +0100

    [add] import of external data structure

--- js/main.js
+++ js/main.js
@@ -1,172 +1,13 @@
 // import contacts from './contacts'
+
+import contacts from './contacts.js';
+
 const {createApp}=Vue
 
 createApp({
   data() {
     return {
-      contacts: [
-        {
-            name: 'Michele',
-            avatar: 'img/avatar_1.jpg',
-            visible: true,
-            messages: [
-                {
-                    date: '10/01/2020 15:30:55',
-                    message: 'Hai portato a spasso il cane?',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 15:50:00',
-                    message: 'Ricordati di stendere i panni',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 16:15:22',
-                    message: 'Tutto fatto!',
-                    status: 'received'
-                }
-            ],
-        },
-        {
-            name: 'Fabio',
-            avatar: 'img/avatar_2.jpg',
-            visible: true,
-            messages: [
-                {
-                    date: '20/03/2020 16:30:00',
-                    message: 'Ciao come stai?',
-                    status: 'sent'
-                },
-                {
-                    date: '20/03/2020 16:30:55',
-                    message: 'Bene grazie! Stasera ci vediamo?',
-                    status: 'received'
-                },
-                {
-                    date: '20/03/2020 16:35:00',
-                    message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                    status: 'sent'
-                }
-            ],
-        },
-        {
-            name: 'Samuele',
-            avatar: 'img/avatar_3.jpg',
-            visible: true,
-            messages: [
-                {
-                    date: '28/03/2020 10:10:40',
-                    message: 'La Marianna va in campagna',
-                    status: 'received'
-                },
-                {
-                    date: '28/03/2020 10:20:10',
-                    message: 'Sicuro di non aver sbagliato chat?',
-                    status: 'sent'
-                },
-                {
-                    date: '28/03/2020 16:15:22',
-                    message: 'Ah scusa!',
-                    status: 'received'
-                }
-            ],
-        },
-        {
-            name: 'Alessandro B.',
-            avatar: 'img/avatar_4.jpg',
-            visible: true,
-            messages: [
-                {
-                    date: '10/01/2020 15:30:55',
-                    message: 'Lo sai che ha aperto una nuova pizzeria?',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 15:50:00',
-                    message: 'Si, ma preferirei andare al cinema',
-                    status: 'received'
-                }
-            ],
-        },
-        {
-            name: 'Alessandro L.',
-            avatar: 'img/avatar_5.jpg',
-            visible: true,
-            messages: [
-                {
-                    date: '10/01/2020 15:30:55',
-                    message: 'Ricordati di chiamare la nonna',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 15:50:00',
-                    message: 'Va bene, stasera la sento',
-                    status: 'received'
-                }
-            ],
-        },
-        {
-            name: 'Claudia',
-            avatar: 'img/avatar_6.jpg',
-            visible: true,
-            messages: [
-                {
-                    date: '10/01/2020 15:30:55',
-                    message: 'Ciao Claudia, hai novità?',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 15:50:00',
-                    message: 'Non ancora',
-                    status: 'received'
-                },
-                {
-                    date: '10/01/2020 15:51:00',
-                    message: 'Nessuna nuova, buona nuova',
-                    status: 'sent'
-                }
-            ],
-        },
-        {
-            name: 'Federico',
-            avatar: 'img/avatar_7.jpg',
-            visible: true,
-            messages: [
-                {
-                    date: '10/01/2020 15:30:55',
-                    message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 15:50:00',
-                    message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                    status: 'received'
-                }
-            ],
-        },
-        {
-            name: 'Davide',
-            avatar: 'img/avatar_8.jpg',
-            visible: true,
-            messages: [
-                {
-                    date: '10/01/2020 15:30:55',
-                    message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                    status: 'received'
-                },
-                {
-                    date: '10/01/2020 15:50:00',
-                    message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 15:51:00',
-                    message: 'OK!!',
-                    status: 'received'
-                }
-            ],
-        }
-      ],
+    contacts,
       counterContact:0,
       newMessageSent:'',
       botAnswers: [
@@ -182,18 +23,15 @@ createApp({
         "Ah, non lo sapevo! Grazie per l'informazione."
       ],
       counterBotAnswer:0,
+      searchInput: '',
+      isSearched:false,
+      isChevronClicked:false,
+      isDisplayedHiddenMenu:false,
+      currentChevron: null,
+      IsMouseEnterChevron:true
 
-    //   // inputdata
-    //   searchedNameInput:'',
-    //   // isSearched:true,
-    //   isInpuFocus:false
-        searchInput: '',
-        isSearched:false,
-        isChevronClicked:false,
-        isDisplayedHiddenMenu:false,
-        currentChevron: null,
-        IsMouseEnterChevron:true
-      
+
+    
     }
   },
 
@@ -263,12 +101,6 @@ createApp({
 
  
 
-    // searchingUserInMiniature(){
-    //   if (!this.serchedNameInput.includes(searchedName())) {
-    //     return 'class="hidden"'
-    //   }
-    // }
-
   },
 
   computed:{
@@ -287,14 +119,6 @@ createApp({
         }
         return this.contacts
       }
-    // computed per vedere se quello che srivo nell' input è incluso nel nome dell' user
-    // searchedContacts(){
-    //   if (this.searchedNameInput === '') {
-    //     return this.contacts;
-    //   } else {
-    //     return this.contacts.filter(contact => contact.name.toLowerCase().includes(this.searchedNameInput.toLowerCase()));
-    //   }
-    // }
 
   },
 
